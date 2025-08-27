23d0772eaeb5a5d060c0c47085a590b803c36419
Akram Ammis <122386669+AkramAmmis@users.noreply.github.com>
2025-08-27 13:15:46 +0200

    Tabellen erstellen, falls sie nicht existieren

--- scripts/transform_mongodb.py
+++ scripts/transform_mongodb.py
@@ -14,7 +14,40 @@ conn = pg8000.connect(
     database='lf8_lets_meet_db',
     user='user',
     password='secret',
-    port=5432
+    port=5433
 )
 cursor = conn.cursor()
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    phone TEXT,
+    friends TEXT[],
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS likes (
+    user_id TEXT REFERENCES users(id),
+    liked_email TEXT,
+    status TEXT,
+    timestamp TIMESTAMP
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS messages (
+    user_id TEXT REFERENCES users(id),
+    conversation_id INT,
+    receiver_email TEXT,
+    message TEXT,
+    timestamp TIMESTAMP
+)
+""")
+
+conn.commit()
+cursor.close()
+conn.close()
