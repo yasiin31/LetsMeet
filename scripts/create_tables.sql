86ee333795b9d83dd36771a551cc365272a10bc7
taha-yasin.ipek <taha-yasin.ipek@hmmh.de>
2025-08-27 14:35:30 +0200

    feat(sql): User-Haupttabelle erstellt

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -10,3 +10,21 @@ CREATE TABLE HOBBY (
     hobby_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE
 );
+
+-- Tabelle für User --
+CREATE TABLE "USER" (
+    user_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone_number VARCHAR(20),
+    birth_date DATE NOT NULL,
+    gender VARCHAR(30),
+    interested_in VARCHAR(30),
+    city_id INT,
+
+    CONSTRAINT fk_city
+                    FOREIGN KEY(city_id)
+                    REFERENCES CITY(city_id)
+                    ON DELETE SET NULL
+);
