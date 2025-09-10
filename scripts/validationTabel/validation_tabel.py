6f98256cb2be3a0649968645fbdc8a7f6c7ff32e
AkramAmmis <akramammis0@gamil.com>
2025-09-10 20:02:35 +0200

    Skript zu überprüfung/validierung der tabbeln in Postgres-DB erstellt

---
+++ scripts/validationTabel/validation_tabel.py
@@ -0,0 +1,95 @@
+from pymongo import MongoClient
+import pg8000
+
+def valiTabel():
+    # MongoDB-Verbindung 
+    client = MongoClient("mongodb://localhost:27018/")
+    db = client["LetsMeet"]
+    collection = db["users"]
+    print("[OK] Verbindung zu MongoDB aufgebaut.")
+
+    # PostgreSQL-Verbindung 
+    conn = pg8000.connect(
+        host='localhost',
+        database='lf8_lets_meet_db',
+        user='user',
+        password='secret',
+        port=5433
+    )
+    cursor = conn.cursor()
+    print("[OK] Verbindung zu PostgreSQL hergestellt.")
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS city (
+        city_id SERIAL PRIMARY KEY,
+        name TEXT,
+        zip_code TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS hobby (
+        hobby_id SERIAL PRIMARY KEY,
+        name TEXT UNIQUE
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id TEXT PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        email TEXT UNIQUE,
+        phone_number TEXT,
+        birth_date DATE,
+        gender TEXT,
+        interested_in TEXT,
+        city_id INT REFERENCES city(city_id),
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS user_hobby (
+        user_id TEXT REFERENCES users(user_id),
+        hobby_id INT REFERENCES hobby(hobby_id),
+        PRIMARY KEY (user_id, hobby_id)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS friendship (
+        user_one_id TEXT REFERENCES users(user_id),
+        user_two_id TEXT REFERENCES users(user_id),
+        created_at TIMESTAMP,
+        PRIMARY KEY (user_one_id, user_two_id)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS likes (
+        like_id SERIAL PRIMARY KEY,
+        liker_id TEXT REFERENCES users(user_id),
+        liked_id TEXT,
+        status TEXT,
+        created_at TIMESTAMP
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS messages (
+        message_id SERIAL PRIMARY KEY,
+        sender_id TEXT REFERENCES users(user_id),
+        receiver_id TEXT,
+        conversation_id INT,
+        message TEXT,
+        sent_at TIMESTAMP
+    )
+    """)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("[OK] Tabellen erstellt oder existieren bereits.")
+    
